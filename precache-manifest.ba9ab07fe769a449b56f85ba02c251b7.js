da1248d110dfcc6a04d80ed8c1d6a40c841673c5
Aseem <aseembhutra.1@gmail.com>
2025-02-09 17:59:43 +0530

    Updates

---
+++ precache-manifest.ba9ab07fe769a449b56f85ba02c251b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6740cf713751b059a4fb47d53141de0e",
+    "url": "/elective-selection-simulation/index.html"
+  },
+  {
+    "revision": "0abc6a496ec0217a706a",
+    "url": "/elective-selection-simulation/static/css/main.526336f6.chunk.css"
+  },
+  {
+    "revision": "07a2e6eaa4b2ff810abf",
+    "url": "/elective-selection-simulation/static/js/2.9cc80c56.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/elective-selection-simulation/static/js/2.9cc80c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb417972276c2eee31d2",
+    "url": "/elective-selection-simulation/static/js/3.a890434d.chunk.js"
+  },
+  {
+    "revision": "0abc6a496ec0217a706a",
+    "url": "/elective-selection-simulation/static/js/main.1ca6c6df.chunk.js"
+  },
+  {
+    "revision": "f8c6d9302b6625eb8f77",
+    "url": "/elective-selection-simulation/static/js/runtime-main.ce384c36.js"
+  }
+]);

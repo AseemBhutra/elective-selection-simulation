bec44f541e90474c9d848def107097431c79324b
Aseem <aseembhutra.1@gmail.com>
2025-02-28 22:05:13 +0530

    improved routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import Welcome from './Welcome';
 import ProgramSelection from './ProgramSelection';
@@ -13,18 +13,14 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/home" element={<Welcome />} />
+        <Route path="/" element={<Welcome />} />
         <Route path="/courses" element={<ProgramSelection />} />
         <Route path="/pgpm" element={<App />} />
         <Route path="/pgdm" element={<PGDM />} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
